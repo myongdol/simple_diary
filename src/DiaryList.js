5d47ebd89bbc05d43778fee0111facff849704e7
myongdol <myongdol132@gmail.com>
2023-07-19 00:41:20 +0900

    [chore] console.log 제거

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,7 +1,8 @@
 import DiaryItem from "./DiaryItem";
 
 const DiaryList = ({diaryList}) => {
-    console.log(diaryList)
+   
+    
     return (
         <div className="DiaryList">
             <h2>일기 목록</h2>
