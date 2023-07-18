2800bab6cb64551cc9ec40e3ed7093e2ab4e06d2
myongdol <myongdol132@gmail.com>
2023-07-19 00:36:26 +0900

    DiaryItem 컴포넌트에서 리스트 보여주기

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,3 +1,5 @@
+import DiaryItem from "./DiaryItem";
+
 const DiaryList = ({diaryList}) => {
     console.log(diaryList)
     return (
@@ -6,12 +8,7 @@ const DiaryList = ({diaryList}) => {
             <h4>{diaryList.length}개의 일기가 있습니다.</h4>
             <div>
                 {diaryList.map((it) => (
-                    <div>
-                        <div>작성자 : {it.author}</div>
-                        <div>내용 : {it.content}</div>
-                        <div>감정 : {it.emtion}</div>
-                        <div>작성 시간 : {it.created_date}</div>
-                    </div>
+                   <DiaryItem key={it.id} {...it}/>
                 ))}
             </div>
         </div>
