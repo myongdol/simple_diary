6d4d5e860dbf192eb1d15bb38d2dd70b4e9c5e13
myongdol <myongdol132@gmail.com>
2023-07-19 00:19:45 +0900

    [feat] 더미데이터터 map사용해서  보여주기

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -3,6 +3,17 @@ const DiaryList = ({diaryList}) => {
     return (
         <div className="DiaryList">
             <h2>일기 목록</h2>
+            <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+            <div>
+                {diaryList.map((it) => (
+                    <div>
+                        <div>작성자 : {it.author}</div>
+                        <div>내용 : {it.content}</div>
+                        <div>감정 : {it.emtion}</div>
+                        <div>작성 시간 : {it.created_date}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
