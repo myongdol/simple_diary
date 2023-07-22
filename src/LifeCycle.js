ddbd1d0701f9bae5cd507b1f49ca72e121115856
myongdol <myongdol132@gmail.com>
2023-07-22 21:55:37 +0900

    [feat] unmount시 실행하는법

--- src/LifeCycle.js
+++ src/LifeCycle.js
@@ -2,40 +2,29 @@ import { useEffect, useState } from "react";
 
 //리액트 라이프 사이클 제어 해보기
 
-const LifeCycle = () => {
 
-    const [count, setCount] = useState(0);
-    const [text, setText] = useState("");
+const UnmountTest = () => {
 
-    useEffect(() => {
+    useEffect(()=> {
         console.log("mount");
+        
+        return () => {
+            //unmount 시점에 실행됨
+            console.log("unmount")
+        }
     },[])
 
-    useEffect(() => {
-        console.log("update");
-    })
+    return <div>언마운트 테스트</div>
+}
 
-    useEffect(() => {
-        console.log(`count is update : ${count}`);
-        if(count > 5) {
-            alert("count가 5를 초과했으므로 초기화됩니다.");
-            setCount(1);
-        }
-    }, [count])
+const LifeCycle = () => {
 
-    useEffect(() => {
-        console.log(`text is update : ${text}`);
-    }, [text])
+    const [isVisible, setIsVisible] = useState(false);
+    const toggle = () => setIsVisible(!isVisible);
 
     return <div style={{padding : 20}}>
-        <div>
-            {count}
-            <button onClick={()=>setCount(count+1)}> + </button>
-        </div>
-        <div>
-            <input value={text} onChange={(e) => setText(e.target.value)}/>
-        </div>
-
+        <button onClick={toggle}> ON / OFF</button>
+        {isVisible && <UnmountTest />}
     </div>
 };
 
