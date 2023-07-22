06c4f98b78c92e220930a9188b542fbeabc4db62
myongdol <myongdol132@gmail.com>
2023-07-22 22:05:56 +0900

    [coment] 필요없는 주석 제거

--- src/App.js
+++ src/App.js
@@ -4,30 +4,6 @@ import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "Myongdol",
-//     content: "hello",
-//     emotion: 3,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "DolMyong",
-//     content: "world",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "Myongdoldol",
-//     content: "hihihihi",
-//     emotion: 1,
-//     created_date: new Date().getTime(),
-//   },
-// ]
-
 //https://jsonplaceholder.typicode.com/comments
 
 function App() {
